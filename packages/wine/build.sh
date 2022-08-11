17975bf5ffacabd04d5a9cc5632c24b7b6bc1201
2096779623 <admin@utermux.dev>
2022-08-11 09:54:55 +0800

    Update build.sh

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -11,7 +11,6 @@ TERMUX_PKG_DEPENDS="ncurses, libpng, libjpeg-turbo, libx11, xorg-util-macros, li
 TERMUX_PKG_HOSTBUILD=true
 
 termux_step_host_build () {
-   $TERMUX_PKG_SRCDIR/configure --without-x --disable-nls --without-pcap --with-freetype --disable-tests LDFLAGS="-l$TERMUX_PREFIX/include/freetype2/freetype/"
-   cat config.log
+   $TERMUX_PKG_SRCDIR/configure --without-x --without-pcap --with-freetype --disable-tests LDFLAGS="-l$TERMUX_PREFIX/include/freetype2/freetype/" || cat config.log
    make -j8
 }
