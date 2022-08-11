7d32889e9d58934421d8c7f4ebf40134294cf432
2096779623 <admin@utermux.dev>
2022-08-11 16:13:15 +0800

    Update build.sh

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -8,7 +8,7 @@ TERMUX_PKG_LICENSE="custom"
 TERMUX_PKG_LICENSE_FILE="LICENSE, COPYING-LIB"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_DEPENDS="ncurses, libpng, libjpeg-turbo, libx11, xorg-util-macros, libxaw, libxt, libxml2, libxslt, libandroid-shmem, xorgproto, libandroid-shmem-static, freetype, freetype-static, binutils"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="enable_wineandroid_drv=no exec_prefix=$TERMUX_PREFIX --without-x --without-pcap --with-freetype --disable-tests --enable-nls --with-wine-tools=$TERMUX_PKG_HOSTBUILD_DIR"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="enable_wineandroid_drv=no exec_prefix=$TERMUX_PREFIX --without-x --without-pcap --with-freetype --disable-tests --enable-nls --with-wine-tools=$TERMUX_PKG_SRCDIR"
 
 env -i PATH="$PATH" sudo apt update -y
 env -i PATH="$PATH" sudo apt install -y libfreetype-dev:i386 libfreetype-dev binutils*
