283a16083769674bc496464e12291004957c9995
Kevin Williams <admin@utermux.dev>
2023-07-02 10:58:07 +0800

    fix(main/dash): conflicts

--- packages/dash/build.sh
+++ packages/dash/build.sh
@@ -3,13 +3,17 @@ TERMUX_PKG_DESCRIPTION="Small POSIX-compliant implementation of /bin/sh"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.5.12
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=http://gondor.apana.org.au/~herbert/dash/files/dash-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=6a474ac46e8b0b32916c4c60df694c82058d3297d8b385b74508030ca4a8f28a
 TERMUX_PKG_ESSENTIAL=true
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-static"
 
-termux_step_post_make_install() {
+termux_step_create_debscripts() {
 	# Symlink sh -> dash
+	cat <<- EOF > ./postinst
+	#!${TERMUX_PREFIX}/bin/sh
 	ln -sfr $TERMUX_PREFIX/bin/{dash,sh}
 	ln -sfr $TERMUX_PREFIX/share/man/man1/{dash,sh}.1
+	EOF
 }
