2dc0a417ba00ca9669600cb59ee88a4614f7c722
Kevin Williams <admin@utermux.dev>
2023-11-26 13:20:52 +0800

    a

--- packages/libarrow-cpp/build.sh
+++ packages/libarrow-cpp/build.sh
@@ -34,7 +34,7 @@ termux_step_pre_configure() {
 	LDFLAGS+=" -landroid-execinfo"
 }
 
-termux_step_post_configure() {
+termux_step_make() {
 	cat /home/builder/.termux-build/libarrow-cpp/build/orc_ep-prefix/src/orc_ep-stamp/orc_ep-build-*.log
 	echo -----------
 	cat /home/builder/.termux-build/libarrow-cpp/build/orc_ep-prefix/src/zlib_ep-stamp/zlib_ep-build-*.log
